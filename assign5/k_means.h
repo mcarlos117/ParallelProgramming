25c928b40639ce9e1478ce2af44c3b3f9c5dc5a8
mcarlos117 <miguelcarlos117@yahoo.com>
2017-07-14 11:59:26 -0500

    Create k_means.h

---
+++ assign5/k_means.h
@@ -0,0 +1,38 @@
+/*
+ * Common definitions for Introduction of Parallel Programming,
+ * Assignment 5: K-Means Algorithm (MPI)
+ *
+ *
+ */
+
+#ifndef __CS4823_ASSIGNMENT_4__
+#define __CS4823_ASSIGNMENT_4__
+
+#define MAX_POINTS 32768
+#define MAX_CENTERS 16
+
+/*
+ * data structure for holding a data point
+ */
+struct point{
+        double x;
+        double y;
+};
+
+/*
+ * read data points from input file
+ */
+int read_points_from_file(char *data_file, struct point *pts, int *m);
+
+/*
+ * find K_means
+ */
+void k_means(struct point p[], int m, int k, int iters, struct point u[],
+            int c[]);
+
+/*
+ * return a (faked) random point
+ */
+struct point random_center(struct point p[]);
+
+#endif
