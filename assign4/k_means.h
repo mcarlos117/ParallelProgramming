434edc33679e49e7e3a70722bc21621cdb43dc4b
mcarlos117 <miguelcarlos117@yahoo.com>
2017-07-14 11:54:02 -0500

    Create k_means.h

---
+++ assign4/k_means.h
@@ -0,0 +1,48 @@
+/*
+ * Common definitions for CS4823 Introduction of Parallel Programming,
+ * Assignment 4: K-Means Algorithm (Pthreads)
+ *
+ */
+
+#ifndef __CS4823_ASSIGNMENT_4__
+#define __CS4823_ASSIGNMENT_4__
+#define MAX_POINTS 32768
+#define MAX_CENTERS 16
+
+/*
+ * data structure for holding a data point
+ */
+struct point{
+        double x;
+        double y;
+};
+
+/*
+ * read data points from input file
+ */
+int read_points_from_file(char *data_file, struct point *pts, int *m);
+
+/*
+ * thread function for K_means
+ */
+void *k_means(void *parameter);
+
+/*
+ * return a (faked) random point
+ */
+struct point random_center(struct point p[]);
+
+/*
+ * TO STUDENTS: Add necessary data structures here
+ */
+struct kMeansParams{
+    int k;
+    int m;
+    int iters;
+    int start;
+    int end;
+};
+
+pthread_barrier_t barrier;
+
+#endif
